6e113fcdb0e3a74033f441b8efd7e4e4bde884fd
AKrasovskiy <pojuratel6@gmail.com>
2016-05-12 17:09:06 +0300

    Добавил задания 9.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-#git-rabota
+#Write a JavaScript program to calculate multiplication and division of two numbers (input from user)
+
+*Поделить и умножить 2 числа с помощью скрипта.
--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,13 @@
-/**
- * Created by Best on 12.05.2016.
- */
+function multiply()
+{
+    num1 = document.getElementById("number_1").value;
+    num2 = document.getElementById("number_2").value;
+    document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divide()
+{
+    num1 = document.getElementById("number_1").value;
+    num2 = document.getElementById("number_2").value;
+    document.getElementById("result").innerHTML = num1 / num2;
+}
