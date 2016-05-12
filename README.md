25023f553b3c9f31a9abfeae892a25f8f6bd8ccb
AKrasovskiy <pojuratel6@gmail.com>
2016-05-12 15:07:04 +0300

    Задания 4.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-#git-rabota
+#Write a JavaScript program to rotate the string 'w3resource' in right direction by periodically removing one letter from the end of the string and attaching it to the front.
+
+*Текст "Текстируем" делает прокрутку.
--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,10 @@
-/**
- * Created by Best on 12.05.2016.
- */
+function animate_text(id) {
+    var element = document.getElementById(id);
+    var textNode = element.childNodes[0];
+    var text = textNode.data;
+
+    setInterval(function () {
+        text = text[text.length - 1] + text.substring(0, text.length - 1);
+        textNode.data = text;
+    }, 100);
+}
