fc5cdc30cdf6dc78a74acd0edf276918f3be3954
AKrasovskiy <pojuratel6@gmail.com>
2016-05-12 14:44:11 +0300

    Задания 3.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-#git-rabota
+#Find the area of a triangle where lengths of the three of its sides are 5, 6, 7.
+
+*Узнать периметр треугольника. Стороны: 5, 6, 7.
--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,6 @@
-/**
- * Created by Best on 12.05.2016.
- */
+var side_1 = 5;
+var side_2 = 6;
+var side_3 = 7;
+var perimeter = (side_1 + side_2 + side_3) / 2;
+var area = Math.sqrt(perimeter * ((perimeter - side_1) * (perimeter - side_2) * (perimeter - side_3)));
+document.write(area);
