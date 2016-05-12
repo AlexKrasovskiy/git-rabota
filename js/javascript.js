572edfef73d2e57e7accbd36e27fe6e5dabc198f
AKrasovskiy <pojuratel6@gmail.com>
2016-05-12 16:10:52 +0300

    Задания 7.

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,7 @@
-/**
- * Created by Best on 12.05.2016.
- */
+var number = Math.floor(Math.random() * 10);
+var guest_number = prompt('Испытайте удачу, веддите число от 1 до 10');
+
+if (guest_number == number)
+    alert('Верно! Поздравляем, это было число:' + number);
+else
+    alert('Не верно, попробуйте в следующий раз. Это было число: ' + number);
