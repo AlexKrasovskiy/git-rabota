dc08571db758b64214a94548ba928a32dff04975
AKrasovskiy <pojuratel6@gmail.com>
2016-05-13 15:51:02 +0300

    Задания functions 14.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-#git-rabota
+#Write a JavaScript function to convert an amount to coins.
+
+*Указать число и предложить варианты чисел для конвертации.
--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,16 @@
-/**
- * Created by Best on 12.05.2016.
- */
+function amount_coins(amount, coins) {
+    if (amount === 0) {
+        return [];
+    }
+    else {
+        if (amount >= coins[0]) {
+            left = (amount - coins[0]);
+            return [coins[0]].concat(amount_coins(left, coins));
+        }
+        else {
+            coins.shift();
+            return amount_coins(amount, coins);
+        }
+    }
+}
+document.write(amount_coins(55, [25, 10, 5]));
