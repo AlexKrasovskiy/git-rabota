c7ce3dd2bbf1b9931aa7d091e0b33c7174415557
AKrasovskiy <pojuratel6@gmail.com>
2016-05-13 14:18:48 +0300

    Задания functions 8.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-#git-rabota
+#Write a JavaScript function that accepts a number as a parameter and check the number is prime or not.
+
+*Проверяет простое число или нет.
--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,28 @@
-/**
- * Created by Best on 12.05.2016.
- */
+function number_prime(n)
+{
+
+    if (n===1)
+    {
+        return false;
+    }
+    else if(n === 2)
+    {
+        return true;
+    }else
+    {
+        for(var x = 2; x < n; x++)
+        {
+            if(n % x === 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+document.write('Простое число - true, если нет - false. Число:   ');
+document.write(number_prime(41));
+
+
+
