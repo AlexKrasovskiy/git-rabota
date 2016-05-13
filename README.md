b025d1c55813406d3a4ba4bccf1cae5c3878ec1b
AKrasovskiy <pojuratel6@gmail.com>
2016-05-13 11:48:52 +0300

    Задания functions 5.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-#git-rabota
+#Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string in upper case.
+
+*Вывод строки и поменять первую букву каждого слова на заглавную.
--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,13 @@
-/**
- * Created by Best on 12.05.2016.
- */
+function uppercase(str)
+{
+    var array = str.split(' ');
+    var newarray = [];
+
+    for(var x = 0; x < array.length; x++)
+    {
+        newarray.push(array[x].charAt(0).toUpperCase()+array[x].slice(1));
+    }
+    return newarray.join(' ');
+}
+document.write(uppercase("пробная строка для теста задания"));
+
