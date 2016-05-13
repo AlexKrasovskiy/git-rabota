69cbe95a781011313aff6f1d686293f8c4fd8c5d
AKrasovskiy <pojuratel6@gmail.com>
2016-05-13 12:07:47 +0300

    Задания functions 6.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-#git-rabota
+#Write a JavaScript function that accepts a string as a parameter and find the longest word within the string
+
+*Оставляет самое длинное слово в строке.
--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,15 @@
-/**
- * Created by Best on 12.05.2016.
- */
+function long_word(str)
+{
+    var array = str.match(/\w[a-z]{0,}/gi); 
+    var result = array[0];
+
+    for(var x = 1 ; x < array.length ; x++)
+    {
+        if(result.length < array[x].length)
+        {
+            result = array[x];
+        }
+    }
+    return result;
+}
+document.write(long_word('Give me pls looooooooongword'));
